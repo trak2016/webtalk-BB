68de8bd10b34493d9065ff76222b683552ecf52b
dzbyrad <dzbyrad@pgs-soft.com>
2015-10-13 11:04:32 +0200

    no message

--- index.js
+++ index.js
@@ -2,11 +2,32 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var clients = {};
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
+
+  socket.on('add-user', function(data) {
+    clients[data.username] = {
+      "socket": socket.id
+    };
+
+    console.log(new Date() + '::Add new user: ' + data.username);
+  });
+
+  //Removing the socket on disconnect
+  socket.on('disconnect', function() {
+  	for(var name in clients) {
+  		if(clients[name].socket === socket.id) {
+  			delete clients[name];
+  			break;
+  		}
+  	}
+  })
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
