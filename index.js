8121be537563751cc2955ce0a335e027f1f4da1c
Dariusz Zbyrad <dariusz.zbyrad@gmail.com>
2015-11-14 19:27:23 +0100

    Add send message about new user

--- index.js
+++ index.js
@@ -17,13 +17,14 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 
   socket.on('add-user', function(data) {
-    if (clients[data.username] == undefined) {
+    if (clients[data.username] == undefined) { //if user name is unique
       clients[data.username] = {
         'socket': socket.id
       };
       self.sendInfoNewUser(socket.id, 'added');
+      self.sendMessageToAllWithoutSpecyficUser(data.username, 'server', data.username + " joined us!");
       console.log(new Date() + '::Add new user: ' + data.username);
-    } else {
+    } else {  //if user name already exists
       self.sendInfoNewUser(socket.id, 'exist');
       console.log(new Date() + '::User ' + data.username + ' already exists');
     }
@@ -44,8 +45,18 @@ io.on('connection', function(socket){
   });
 });
 
+//Send status to the user who wants to join
 self.sendInfoNewUser = function(socketId, msg) {
   io.sockets.connected[socketId].emit('new user info', msg);
+}
+
+//Send message to all users without specyfic user
+self.sendMessageToAllWithoutSpecyficUser = function(excludedUserName, author, msg) {
+  for(var name in clients) {
+    if(name != excludedUserName) {
+      io.sockets.connected[clients[name].socket].emit('message', {'msg': msg, 'author': author});
+    }
+  }
 }
 
 http.listen(3000, function(){
--- resources/js/MainController.js
+++ resources/js/MainController.js
@@ -18,17 +18,33 @@ controllers.controller('MainCtrl', ['$scope', '$http', function ($scope, $http) 
     };
 
     $scope.onSubmitMessage = function() {
+      //Send message to all users
       socket.emit('chat message', $scope.message);
       $scope.message = '';
     };
 
     $scope.init = function() {
+      //Receive chat message
       socket.on('chat message', function(msg){
+        console.log("New message: " + msg);
         $scope.messages.push(msg);
         $scope.$apply();
       });
 
-      //Message from server about new user
+      //TODO: merge with 'chat message' listener
+      socket.on('message', function(data){
+        console.log('New message from ' + data.author + ': ' + data.msg);
+
+        var message = data.msg;
+        if ('server' !== data.author) {
+          message = data.author + ': ' + message;
+        }
+
+        $scope.messages.push(message);
+        $scope.$apply();
+      });
+
+      //Message with status from server about new user
       socket.on('new user info', function(msg){
         if ('added' === msg) { // Remove login form and show the chat form
           $scope.showUserLoginForm = false;
